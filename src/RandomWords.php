1565b2a7429e5d795202e31f93a57fcfcbb870c5
Jan Östlund <janne@eastwest.se>
2019-03-08 10:43:59 +0100

    wip

--- src/RandomWords.php
+++ src/RandomWords.php
@@ -5,10 +5,35 @@ namespace Eastwest\RandomWords;
 
 class RandomWords
 {
-    public function get()
-    {
-        $fileContents = file(dirname(__FILE__) . '/../data/alpha-words.txt');
+    protected $addRandomString = false;
+    protected $randomStringLength = 4;
 
-        return $fileContents[rand(0, count($fileContents) - 1)];
+    public function addRandomString($length = 4) {
+        $this->addRandomString = true;
+        $this->addRandomStringLength = $length;
+
+        return $this;
+    }
+
+    public function get($prefix = null)
+    {
+        $parts = [];
+
+        if($prefix) {
+            $parts[] = $prefix;
+        }
+
+        $adjectivesContent = file(dirname(__FILE__) . '/../data/colors.txt');
+        $nonunContent = file(dirname(__FILE__) . '/../data/music_theory.txt');
+
+        $parts[] = trim($adjectivesContent[rand(0, count($adjectivesContent) - 1)]);
+        $parts[] = trim($nonunContent[rand(0, count($nonunContent) - 1)]);
+
+        if($this->addRandomString == true) {
+            $parts[] = str_random($this->randomStringLength);;
+        }
+
+
+        return str_slug(implode('-', $parts));
     }
 }
